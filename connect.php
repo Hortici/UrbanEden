8b68dce18607cbf52c93dba5492c4b4384f59d5d
Marko Faletar <faletar.marko@gmail.com>
2024-05-03 10:21:25 +0200

    vratio nazad php komentare koji su bili nestali

--- connect.php
+++ connect.php
@@ -1,12 +1,15 @@
 <?php
 
+//podatci potrebni za spajanje na bazu
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "urbanedenbaza";
 
+//spajanje na bazu
 $connected = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, 4306);
 
+//spajanje nije uspjelo
 if(!$connected){
     die("Nije uspjelo spajanje");
 }
