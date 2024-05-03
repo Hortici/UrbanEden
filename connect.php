97d6c74ae19dd61c10465f554c8571148af221b6
DBan3 <dbban53@gmail.com>
2024-05-03 09:07:53 +0200

    prebacio sve promjene iz foldera sql u github

--- connect.php
+++ connect.php
@@ -1,15 +1,12 @@
 <?php
 
-/*Podatci koji su potrebni za spajanje na bazu podataka*/
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "urbanedenbaza";
 
-/*Spajanje na bazu podataka*/
 $connected = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, 4306);
 
-/*Spajanje nije uspjelo*/
 if(!$connected){
     die("Nije uspjelo spajanje");
 }
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-/*Započinje sesiju na trenutnoj stranici
-ukoliko je korisnik ulogiran preusmjerava ga se na home.php stranicu*/
 session_start();
 if(isset($_SESSION["korisnik"])){
     header("Location: pages/home.php");
-    /*Varijabla linkLogo služi za kontrolu navigacije u headeru*/
     $linkLogo = 'home.php';
 }else{
     $linkLogo = 'index.php';
@@ -24,17 +21,16 @@ if(isset($_SESSION["korisnik"])){
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <!--Link za korištenje Bootstrapa-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--Link za korištenje Bootstrap ikona, Boostrap Icon Font-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!--Link za korištenje Bootstrap ikona, Boostrap Icon Font-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
   <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
 
   <!--Link za Bootstrap js module-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </head>
 
@@ -48,8 +44,9 @@ if(isset($_SESSION["korisnik"])){
       <!--Navbar sekcija-->
       <nav class="navbar navbar-expand-lg">
         <!--Logo i link za home stranicu-->
-        <a class="navbar-brand" href="index.php">
-          <h1 id="logo">UrbanEden</h1>
+        <a class="navbar-brand d-flex flex-row" href="index.php">
+          <img src="assets/images/tomato.svg" class="px-2" alt="Image of a tomato">
+          <h1 id="logo" class="m-0">UrbanEden</h1>
         </a>
         <!--Nav Toggle gumb-->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
@@ -67,40 +64,27 @@ if(isset($_SESSION["korisnik"])){
             <li class="nav-item"><button type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#LoginExampleModal"><a><i class="bi bi-person-fill fs-2"></i></a></button></li>
           </ul>
         </div>
-
       </nav>
-
-
     </div>
   </header>
 <!-- Glavni dio stranice -->
   <main class="w-75 justify-content-center mx-auto">
 
       <?php
-
-      /*
-       * Kod koji kontrolira login na index stranici
-       * Ukoliko je poslana login forma izvršava se sljedeći dio koda
-       */
       if(isset($_POST["login"])){
-          /*Dohvaća uneseni email i lozinku te ih sprema u varijable*/
           $email = $_POST["email"];
           $lozinka = $_POST["password"];
 
-          //echo $email;
-          //echo $lozinka;
+          echo $email;
+          echo $lozinka;
 
-          /*Uvjetuje da se connect.php koji služi za spajanje na bazu učita*/
           require_once "connect.php";
 
-          /*Uzima sve podatke iz baze gdje se email podudara s unesenim te ih sprema u varijablu pomoću mysqli fetch array*/
           $sql = "SELECT * FROM korisnici WHERE email = '$email'";
           if (isset($connected)) {
               $rezultat = mysqli_query($connected, $sql);
           }
           $korisnik = mysqli_fetch_array($rezultat, MYSQLI_ASSOC);
-          /*Ukoliko jsu podatci uspješno dohvaćeni potvrđuje uneseni password s kriptiranim passwordom iz baze
-          Sprema ime korisnika u session kako bi zapamtio da je korisnik registriran*/
           if($korisnik){
               if(password_verify($lozinka, $korisnik["password"])){
 
@@ -110,15 +94,12 @@ if(isset($_SESSION["korisnik"])){
                       <a href="logout.php" class="btn btn-warning">Log out</a>
                   */
 
-                  /* Šalje korisnika na home.php stranicu aplikacije ako je uspješno registriran*/
                   header("Location: pages/home.php");
                   die();
               }else{
-                  /*Ako je korisnik unesao krivu lozinku ispisuje mu se poruka*/
                   echo "<div class='alert alert-danger'>Lozinka nije tocna</div>";
               }
           }else{
-              /*Ako je korisnik unesao email koji ne postoji u bazi ispisuje mu se poruka*/
               echo "<div class='alert alert-danger'>Email ne postoji</div>";
           }
       }
@@ -164,13 +145,7 @@ if(isset($_SESSION["korisnik"])){
   <!-- Kraj modalnog dijela prijave korisnika -->
 
       <?php
-      /*
-       * Kod koji kontrolira registraciju na index stranici
-       * Ukoliko je poslana forma za registraciju izvršava se sljedeći dio koda
-       */
       if (isset($_POST["registracija"])) {
-          /*Dohvaća uneseno ime,email,lozinku te ponovljenu lozinku te ih sprema u varijable,
-          kreira kriptiranu lozinku u posebnoj varijabli*/
           $ime2 = $_POST["ime2"];
           $email2 = $_POST["email2"];
           $lozinka2 = $_POST["password2"];
@@ -178,8 +153,6 @@ if(isset($_SESSION["korisnik"])){
 
           $lozinkaHash2 = password_hash($lozinka2, CRYPT_BLOWFISH);
 
-          /*Stvara error array koji provjerava ukoliko je korisnik zaboravio unijeti neku od informacije ili je u nepravilnom formatu
-          te sprema poruke u taj array*/
           $error = array();
           if (empty($ime2) or empty($email2) or empty($lozinka2) or empty($lozinka22)) {
               array_push($error, "Treba unijeti sva polja");
@@ -194,27 +167,20 @@ if(isset($_SESSION["korisnik"])){
               array_push($error, "Lozinke se ne podudaraju");
           }
 
-          /*Uvjetuje da se connect.php koji služi za spajanje na bazu učita*/
           require_once "connect.php";
-
-          /*Uzima sve podatke iz baze gdje se email podudara s unesenim te ih sprema u varijablu pomoću mysqli fetch array*/
           $sql = "SELECT * FROM korisnici WHERE email = '$email2'";
           if (isset($connected)) {
               $rezultat = mysqli_query($connected, $sql);
           }
           $rowCount = mysqli_num_rows($rezultat);
-          /*Ukoliko poslani query vrati redak iz baze to znači da korisnik s tim emailom već postoji i šalje novu poruku u error array*/
           if ($rowCount > 0) {
               array_push($error, "Email vec postoji");
           }
-          /*Ukoliko postoje error poruke ispisuju se korisniku*/
           if (count($error) > 0) {
               foreach ($error as $err) {
                   echo "<div class='alert alert-danger'>$err</div>";
               }
           } else {
-              /*Stvara query za unos podataka u bazu, provjerava konekciju te izvršava unos podataka
-              pomoću mysqli_stmt_execute, podatci su na ovaj način zaštićeni*/
               $sql = "INSERT INTO korisnici (ime, email, password) VALUES (?,?,?)";
               $stmt = mysqli_stmt_init($connected);
               $prepare = mysqli_stmt_prepare($stmt, $sql);
@@ -223,7 +189,6 @@ if(isset($_SESSION["korisnik"])){
                   mysqli_stmt_execute($stmt);
                   echo "<div class='alert alert-success'> Registriran si!</div>";
               } else {
-                  /*Unos podatak nije bio uspješan*/
                   die("Registracija nije uspjela");
               }
           }
@@ -286,7 +251,7 @@ if(isset($_SESSION["korisnik"])){
 
 <!-- Glavni sadržaj stranice -->  
   <div class="opis d-flex flex-row">
-    <div class="left w-50 m-5 flex-column">
+    <div class="left w-50 m-5 flex-column container-fluid mh-50">
       <h1>Tvoj Vrtlarski pomoćnik</h1>
       <br>
       <p class="my-5 fs-4">Želiš imati svoj vrt, ali ne znaš gdje početi? <br>
@@ -295,12 +260,12 @@ if(isset($_SESSION["korisnik"])){
       <p class="my-5 fs-6 ">Sve osnovne informacije o uzgoju biljaka na jednom mjestu.
       </p>
       </div>
-    <div class="right bg-secondary w-50 h-auto rounded-3">
-      <img src="assets/images/Gardening-rafiki 1.svg" alt="Gardening picture">
+    <div class="right container-xlg rounded-3">
+      <img src="assets/images/Gardening-rafiki 1.svg" class="img-fluid" alt="Gardening picture">
     </div>
   </div>
-
-    <div class="processSumUp d-flex flex-column mt-5 pt-5">
+      <!-- Objašnjeni proces stranice -->
+    <div class="processSumUp d-flex flex-column my-5 py-5">
       <div class="naslovi d-flex flex-row justify-content-between">
         <span class="px-2 mx-3 col-3 fs-5">
           <h3 class="mb-4">
@@ -321,30 +286,42 @@ if(isset($_SESSION["korisnik"])){
           <p>Podsjetnici za redovitu brigu i kalendari radova u vrtu.</p>
         </span>
       </div>
-
+      <!-- Slike za objašnjeni proces -->
       <div class="slike d-flex flex-row justify-content-between fs-2 mt-3 h-50">
-        <div class="d-flex flex-column bg-secondary w-50 h-auto rounded-3 mx-4 px-2">
-          <i class="bi bi-dot" ></i>
-          <i class="bi bi-dot" ></i>
-          <i class="bi bi-dot"></i>
+        <div class="card h-25 w-25 ms-3 h-auto mt-3" aria-hidden="true" style="min-height: 200px;">
+          <img src="assets/images/Seeding-bro.svg" alt="Planting a garden">
         </div>
-        <div class="d-flex flex-column bg-secondary w-50 rounded-3 mx-4 px-2">
-          <i class="bi bi-dot"></i>
-          <i class="bi bi-dot"></i>
-          <i class="bi bi-dot"></i>
+        <div class="card h-25 w-25 h-auto mt-3" aria-hidden="true" style="min-height: 200px;">
+          <img src="assets/images/potted plants-bro.svg" alt="potted plants">
         </div>
-        <div class="d-flex flex-column bg-secondary w-50 rounded-3 mx-4 px-2">
-          <i class="bi bi-dot"></i>
-          <i class="bi bi-dot"></i>
-          <i class="bi bi-dot"></i>
+        <div class="card h-25 w-25 me-3 h-auto mt-3" aria-hidden="true" style="min-height: 200px;">
+          <img src="assets/images/Reminders-bro.svg" alt="Reminders">
         </div>
       </div>
-
     </div>
-  </div>
+    <!-- Poziv na pridruživanje -->
+    <section class="my-5 py-5 d-flex flex-column justify-content-center align-items-center">
+      <h2 class="fs-1"><strong>Zvuči dobro?</strong></h2>
+      <p class="my-5 fs-4">Postani član UrbanEden zajednice!</p>
+      <button type="submit" name="registracija" class="btn btn-primary btn-success rounded-4 fs-4 px-4 py-3"data-bs-toggle="modal" data-bs-target="#RegisterExampleModal">Registriraj se</button>
+    </section>
     
 
   </main>
+  <!-- Footer sa nav elementima i  copyrightom -->
+  <footer class="position-relative bottom-0 d-flex flex-column align-items-center justify-content-between" style="background-color:#005A00; min-height:100px;">
+        <div class="container-fluid w-50 px-0 pt-2 d-flex flex-row align-items-center justify-content-center ">
+            <span class="mb-2">
+                <ul class="nav d-flex flex-row align-items-center">
+                    <li class="nav-link px-5"><a href="aboutUs.html" class="nav-link text-white p-0">O nama</a></li>
+                    <li class="nav-link px-5"><a href="policy.html" class="nav-link text-white p-0">Politika privatnosti</a></li>
+                </ul>
+            </span>
+        </div>
+        <span class="text-white mt-3 pb-3">
+                &copy; UrbanEden 2024
+         </span>
+    </footer>
 
 
 
