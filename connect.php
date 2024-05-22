1bab8248bf1235debb307882d146ea3ec57cfb21
Marko Faletar <faletar.marko@gmail.com>
2024-05-22 18:19:26 +0200

    added secure connection to azure database

--- connect.php
+++ connect.php
@@ -2,13 +2,28 @@
 session_start();
 
 /*podatci potrebni za spajanje na bazu*/
+/*
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "urbanedenbaza";
+*/
 
-/*spajanje na bazu*/
+$hostName = "urban-eden-server1.mysql.database.azure.com";
+$dbUser = "ueAdmin";
+$dbPassword = "theUrbanAd123";
+$dbName = "urbanedenbaza";
+$sslmode="require";
+
+$connected = mysqli_init();
+mysqli_ssl_set($connected,NULL,NULL, "bazaExport/DigiCertGlobalRootCA.pem", NULL, NULL);
+mysqli_real_connect($connected, $hostName, $dbUser, $dbPassword, $dbName, 3306, MYSQLI_CLIENT_SSL);
+
+
+/*
 $connected = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, 4306);
+*/
+/*spajanje na bazu*/
 
 /*spajanje nije uspjelo*/
 if(!$connected){
