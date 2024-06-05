e3a496c3d2874ee6ab9f3adac9942c5d3186d5a6
Marko Faletar <faletar.marko@gmail.com>
2024-06-05 12:47:58 +0200

    ererawdasd103

--- pages/connect.php
+++ pages/connect.php
@@ -13,7 +13,7 @@ $dbName = "urbanedenbaza";
 $sslmode="require";
 
 $connected = mysqli_init();
-mysqli_ssl_set($connected,NULL,NULL, "bazaExport/DigiCertGlobalRootCA.pem", NULL, NULL);
+mysqli_ssl_set($connected,NULL,NULL, "../bazaExport/DigiCertGlobalRootCA.pem", NULL, NULL);
 mysqli_real_connect($connected, $hostName, $dbUser, $dbPassword, $dbName, 3306, MYSQLI_CLIENT_SSL);
 
 
