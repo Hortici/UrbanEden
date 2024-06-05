402407f0d0644c58a98dabfaa48ab9c7cdd04dba
Marko Faletar <faletar.marko@gmail.com>
2024-06-05 12:16:38 +0200

    ererawdasd97

--- pages/about.php
+++ pages/about.php
@@ -136,7 +136,7 @@ if (isset($_POST["odjava"])) {
             <?php
             //require_once "../connect.php";
             try {
-                require_once $_SERVER["DOCUMENT_ROOT"]."/connect.php";
+                require_once $_SERVER["DOCUMENT_ROOT"]."/connet.php";
                 if (isset($connected)) {
                     $sql = "SELECT * FROM biljke_info ORDER BY id DESC";
                     $rows_plants = mysqli_query($connected, $sql);
