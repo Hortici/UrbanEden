feebe69dffee9b499729b03d8a95556c23199cf0
DBan3 <dbban53@gmail.com>
2024-06-09 13:36:54 +0200

    Added funcionality of adding plants
    treba doradit layout ali radi, ima nesto js-a da se može integrigat sa bazom

--- index.php
+++ index.php
@@ -21,18 +21,12 @@ if(isset($_SESSION["korisnik"])){
   <link rel="stylesheet" href="style.css">
   <!--Font Awesome Link-->
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  <!--Link za korištenje Bootstrapa-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
   <!--Link za korištenje Bootstrap ikona, Boostrap Icon Font-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
   <script type="module" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule="" src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js"></script>
-
-  <!--Link za Bootstrap js module-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </head>
 
