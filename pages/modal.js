fdcded04cc5a80fb3c056a983f3e48fb9938ac93
DBan3 <dbban53@gmail.com>
2024-06-10 09:17:12 +0200

    added crop about file
    crop about je info stranica od blijke

--- pages/modal.js
+++ pages/modal.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() { 
 
-
-    let plantModal = this.document.querySelectorAll("#plant");
+    //Data manipulation for adding plants and opening their showcase before adding
+    let plantModal = document.querySelectorAll("#plant");
 
     plantModal.addEventListener("click",(e) => {
         let name;
@@ -22,8 +22,27 @@ window.addEventListener('DOMContentLoaded', function() {
         let quantityCol;
 
         
+            this.window.location.href("/cropAbout.php");
+           
+        
     })
 
+    //about page
+    //listen for click on card
+    
+    let uvjetiPage = this.document.getElementById("uvijeti");
+    uvjetiPage.addEventListener("click", (e) => {
+        this.window.location.reload();
+    })
+
+    let zdravljePage = this.document.getElementById("zdravlje"); 
+    zdravljePage.addEventListener("click", (e) => {
+        this.document.querySelector("article").innerHTML = '';
+        this.document.querySelector("article").innerHTML =
+            '<h3>Lisne uši</h3>'
+            '<p>Blitva ima veliku mogućnost prilagodbe tlu i klimi. Minimalna je temperatura klijanja 5 °C, a optimalna 16 - 24 °C. Pri nižim temperaturama 5 - 10 °C biljka sporo raste, a optimalna temperatura rasta je 16 - 20 °C. I mlada i potpuno razvijena biljka može podnijeti blage mrazove.</p>';
+        
+    })
 
 
 
